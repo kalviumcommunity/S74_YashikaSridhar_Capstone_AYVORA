41ed5f0b8f916286484b49fed24d9c95447a8fcd
Yashi <farid@Yashis-MacBook-Pro.local>
2025-05-12 10:08:01 +0530

    menuandnavbar

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Satisfy your cravings with AYVORA—delivering delicious meals from top restaurants straight to your door, anytime, anywhere!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
